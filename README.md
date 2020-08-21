b1582bac7f205a4b229167468cf7e41f2d412c5f
phgrosjean <phgrosjean@sciviews.org>
2020-08-21 15:21:36 +0200

    Update of the book to use learndown + prepare version 2020

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Science des données biologiques, UMONS - Le book, partie 2 !
+# Science des données biologiques II
 
-The source files for the SDD-UMONS2 book, written using [bookdown](https://bookdown.org/home/about.html). You can find this book at http://biodatascience-course.sciviews.org/sdd-umons/
+The source files for the SDD-UMONS2 book edition 2020-2021, written using [bookdown](https://bookdown.org/home/about.html) and [learndown](https://www.sciviews.org/learndown/). You can find this book at https://wp.sciviews.org/sdd-umons2.
 
-The content of this book is licensed 
+The content of this book is licensed under 
 [CC BY-NC-SA 4.0](https://creativecommons.org/licenses/by-nc-sa/4.0/deed.fr).
---
+++ setup.R
@@ -0,0 +1,42 @@
+# Learndown configuration and functions
+learndown <- list(
+  baseurl = "https://wp.sciviews.org", # The base URL for the site
+  shiny_imgdir = "images/shinyapps",   # The Shiny image directory (screenshots)
+  svbox = 2020,                        # The SciViews Box version used
+  institutions = "UMONS",              # Known institutions
+  courses = c(
+    "S-BIOG-015", "S-BIOG-061",        # SDD2 Q1 & Q2
+    "S-BIOG-940"                       # SDD2 Charleroi Q1 & Q2
+  )
+)
+
+h5p <- function(id, ...)
+  learndown::h5p(id, baseurl = learndown$baseurl, ...)
+
+launch_shiny <- function(url, fun = "BioDataScience::runApp",
+  #ENalt1 = "*Click to start the Shiny application*",
+  alt1 = "*Cliquez pour lancer l'application Shiny*",
+  #ENalt2 = paste0(
+  #EN  "*Click to start or run `{fun}(\"{app}\")` in the svbox",
+  #EN  learndown$svbox, ".*"), ...)
+  alt2 = paste0(
+    "*Cliquez pour lancer ou exécutez `{fun}(\"{app}\")` dans la svbox",
+    learndown$svbox, ".*"), ...)
+  learndown::launch_shiny(url = url, imgdir = learndown$shiny_imgdir, fun = fun,
+    alt1 = alt1, alt2 = alt2, ...)
+
+launch_learnr <- function(url, fun = "BioDataScience::run", ...)
+  launch_shiny(url = url, fun = fun, ...)
+
+# Include javascript and css code for {learndown} additional features
+# in style.css and header.html, respectively
+learndown::learndown_init(
+  baseurl = learndown$baseurl,
+  #EN hide.code.msg = "See the code",
+  hide.code.msg = "Voir le code",
+  institutions = learndown$institutions,
+  courses = learndown$courses)
+
+
+# Knitr default options
+knitr::opts_chunk$set(comment = "#", fig.align = "center")
