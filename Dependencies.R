cac94b12a2a2ccec3b22e8b6acea5893b3defa23
phgrosjean <phgrosjean@sciviews.org>
2021-02-14 17:32:36 +0100

    Dependencies added

---
+++ Dependencies.R
@@ -0,0 +1,61 @@
+# This bookdown in its version 2020 needs the following R packages
+#  R 3.6.3
+# Use the following to list all demendencies:
+#imports <- unique(c("bookdown", "learndown", "SciViews",
+#  attachment::att_from_rmds(".")))
+#attachment::att_to_desc_from_is(path.d = "DESCRIPTION",
+#  imports = imports, suggests = NULL)
+
+# From CRAN
+install.packages(c("knitr", "bookdown", # The bases!
+  "gdtools", "svglite", # SVG graphs
+  "htmltools", "vembedr", # To embed videos easily
+  "devtools", # To install packages from Github
+  ape,
+  broom,
+  ca,
+  car,
+  dbplyr,
+  dplyr,
+  factoextra,
+  FactoMineR,
+  fastcluster,
+  flashClust,
+  GGally,
+  ggdendro,
+  ggfortify,
+  ggrepel,
+  grid,
+  gridExtra,
+  iterators,
+  knitr,
+  kohonen,
+  MASS,
+  modelr,
+  multcomp,
+  naniar,
+  plotly,
+  purrr,
+  RColorBrewer,
+  rgl,
+  rlang,
+  RSQLite,
+  scales,
+  SciViews,
+  sessioninfo,
+  skimr,
+  stats,
+  tibble,
+  tidyverse,
+  vegan,
+  vembedr,
+  viridis
+))
+
+# From Github (latest devel version)
+devtools::install_github("SciViews/flow")
+devtools::install_github("SciViews/data")
+devtools::install_github("SciViews/chart")
+devtools::install_github("SciViews/SciViews")
+devtools::install_github("SciViews/learndown")
+devtools::install_github("rstudio/bookdown")
